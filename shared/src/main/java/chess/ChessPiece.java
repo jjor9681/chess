84baacced26c62c0a57aa0d51aac4d9bedd073e5
Jonas <jjonasjorgensen@gmail.com>
2026-05-08 21:51:13 -0600

    The vast majority of my pawn sections works now. Just having trouble capturing from the starting line.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -275,7 +275,206 @@ public class ChessPiece {
             return validMoves;
         }
         if (piece.getPieceType() == PieceType.PAWN){
+            // Pawn is significantly more complicated. I'll use an if statement to separate team white and team black.
+            // White pawns advance to the north. The north square needs to be empty, and the northeast/northwest
+            // squares need to have an enemy. Then check if the white piece is on row 2. Then do the same thing but
+            // facing the south and checking row 7 for the double move.
 
+            // White pawn
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+
+                // Check square in front of the pawn
+                int r = myPosition.getRow() + 1;
+                int c = myPosition.getColumn();
+
+                ChessPosition pos = new ChessPosition(r, c);
+
+                // pawn can't capture forward. If something is in the way, too bad!
+                if (board.getPiece(pos) == null) {
+
+                    // Check for promo
+                    if (r == 8) {
+
+                        validMoves.add(new ChessMove(myPosition, pos, PieceType.QUEEN));
+                        validMoves.add(new ChessMove(myPosition, pos, PieceType.ROOK));
+                        validMoves.add(new ChessMove(myPosition, pos, PieceType.BISHOP));
+                        validMoves.add(new ChessMove(myPosition, pos, PieceType.KNIGHT));
+                    }
+                    else {
+                        validMoves.add(new ChessMove(myPosition, pos, null));
+                    }
+
+                    // double move
+                    if (myPosition.getRow() == 2) {
+
+                        ChessPosition bonusPos = new ChessPosition(myPosition.getRow() + 2, myPosition.getColumn());
+
+                        // Both spaces must be empty.
+                        if (board.getPiece(bonusPos) == null) {
+                            // Check for promo
+                            if (r == 8) {
+                                validMoves.add(new ChessMove(myPosition, pos, PieceType.QUEEN));
+                                validMoves.add(new ChessMove(myPosition, pos, PieceType.ROOK));
+                                validMoves.add(new ChessMove(myPosition, pos, PieceType.BISHOP));
+                                validMoves.add(new ChessMove(myPosition, pos, PieceType.KNIGHT));
+                            }
+                            else {
+                                validMoves.add(new ChessMove(myPosition, pos, null));
+                            }
+                        }
+                    }
+                }
+
+                // Check north west
+                r = myPosition.getRow() + 1;
+                c = myPosition.getColumn() - 1;
+                if (c >= 1) {
+                    pos = new ChessPosition(r, c);
+                    ChessPiece target = board.getPiece(pos);
+                    if (target != null &&
+                            target.getTeamColor() != piece.getTeamColor()) {
+
+                        // Check for promo
+                        if (r == 8) {
+
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.QUEEN));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.ROOK));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.BISHOP));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.KNIGHT));
+                        }
+                        else {
+                            validMoves.add(new ChessMove(myPosition, pos, null));
+                        }
+                    }
+                }
+
+                // Check North East
+                r = myPosition.getRow() + 1;
+                c = myPosition.getColumn() + 1;
+
+                if (c <= 8) {
+
+                    pos = new ChessPosition(r, c);
+                    ChessPiece target = board.getPiece(pos);
+
+                    if (target != null &&
+                            target.getTeamColor() != piece.getTeamColor()) {
+
+                        // Check for promo
+                        if (r == 8) {
+
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.QUEEN));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.ROOK));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.BISHOP));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.KNIGHT));
+                        }
+                        else {
+                            validMoves.add(new ChessMove(myPosition, pos, null));
+                        }
+                    }
+                }
+            }
+
+            // Black pawn, basically the same as the white but towards the south.
+            else {
+
+                // Check forward one square.
+                int r = myPosition.getRow() - 1;
+                int c = myPosition.getColumn();
+
+                ChessPosition pos = new ChessPosition(r, c);
+
+                if (board.getPiece(pos) == null) {
+
+                    // Check for promo
+                    if (r == 1) {
+
+                        validMoves.add(new ChessMove(myPosition, pos, PieceType.QUEEN));
+                        validMoves.add(new ChessMove(myPosition, pos, PieceType.ROOK));
+                        validMoves.add(new ChessMove(myPosition, pos, PieceType.BISHOP));
+                        validMoves.add(new ChessMove(myPosition, pos, PieceType.KNIGHT));
+                    }
+                    else {
+                        validMoves.add(new ChessMove(myPosition, pos, null));
+                    }
+
+                    // double move.
+                    if (myPosition.getRow() == 7) {
+
+                        ChessPosition bonusPos =
+                                new ChessPosition(myPosition.getRow() - 2,
+                                        myPosition.getColumn());
+
+                        if (board.getPiece(bonusPos) == null) {
+                            // Check for promo
+                            if (r == 1) {
+
+                                validMoves.add(new ChessMove(myPosition, pos, PieceType.QUEEN));
+                                validMoves.add(new ChessMove(myPosition, pos, PieceType.ROOK));
+                                validMoves.add(new ChessMove(myPosition, pos, PieceType.BISHOP));
+                                validMoves.add(new ChessMove(myPosition, pos, PieceType.KNIGHT));
+                            }
+                            else {
+                                validMoves.add(new ChessMove(myPosition, pos, null));
+                            }
+                        }
+                    }
+                }
+
+                // Check south west capture.
+                r = myPosition.getRow() - 1;
+                c = myPosition.getColumn() - 1;
+
+                if (c >= 1) {
+
+                    pos = new ChessPosition(r, c);
+                    ChessPiece target = board.getPiece(pos);
+
+                    if (target != null &&
+                            target.getTeamColor() != piece.getTeamColor()) {
+
+                        // Check for promo
+                        if (r == 1) {
+
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.QUEEN));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.ROOK));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.BISHOP));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.KNIGHT));
+                        }
+                        else {
+                            validMoves.add(new ChessMove(myPosition, pos, null));
+                        }
+                    }
+                }
+
+                // Check south east capture.
+                r = myPosition.getRow() - 1;
+                c = myPosition.getColumn() + 1;
+
+                if (c <= 8) {
+
+                    pos = new ChessPosition(r, c);
+                    ChessPiece target = board.getPiece(pos);
+
+                    if (target != null &&
+                            target.getTeamColor() != piece.getTeamColor()) {
+
+                        // Check for promo
+                        if (r == 1) {
+
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.QUEEN));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.ROOK));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.BISHOP));
+                            validMoves.add(new ChessMove(myPosition, pos, PieceType.KNIGHT));
+                        }
+                        else {
+                            validMoves.add(new ChessMove(myPosition, pos, null));
+                        }
+                    }
+                }
+            }
+
+            return validMoves;
         }
         if (piece.getPieceType() == PieceType.QUEEN){
             // High key this is just a combination of bishop then rook.
