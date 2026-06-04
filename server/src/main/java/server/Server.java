0080f3e43e3060b2e00d60a82f0a5580907e155a
Jonas <jjonasjorgensen@gmail.com>
2026-06-03 22:41:28 -0600

    Looked through petshop and am leaving comments where I would like to start working.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -14,6 +14,10 @@ public class Server {
         // Okay i am looking through petshop and the textbook to make sure I understand how things work.
         // The lecture videos were good and helped a lot.
 
+        // I think this is a good place to start.
+        // I can also go and add all those folders across the project that are going to
+        // be required later in the project.
+
     }
 
     public int run(int desiredPort) {
