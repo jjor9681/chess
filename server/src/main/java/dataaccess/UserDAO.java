123214c8fc96bdf0f2b511744b4eb4267aa1894a
Jonas <jjor9681@byu.edu>
2026-06-08 17:08:18 -0600

    Added three methods to UserDAO for clear, createUser, and getUser.

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -4,4 +4,9 @@ package dataaccess;
 import model.UserData;
 
 public interface UserDAO {
+    void clear() throws DataAccessException;
+
+    void createUser(UserData user) throws DataAccessException;
+
+    void getUser(String username) throws  DataAccessException;
 }
