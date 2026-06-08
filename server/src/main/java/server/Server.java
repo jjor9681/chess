a2bef87f7ac3ca056abc6d0afcadfa0bd9dd5002
Jonas <jjor9681@byu.edu>
2026-06-08 14:42:07 -0600

    I feel like the model layer is critical to anything else working, so that is what I am \developing first.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -9,19 +9,6 @@ public class Server {
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
-        // Register your endpoints and exception handlers here.
-
-        // Okay i am looking through petshop and the textbook to make sure I understand how things work.
-        // The lecture videos were good and helped a lot.
-
-        // I think this is a good place to start.
-        // I can also go and add all those folders across the project that are going to
-        // be required later in the project.
-
-        // Refering back to my project 2 to understand what I need to do.
-
-        // Should be good to start here.
-
     }
 
     public int run(int desiredPort) {
