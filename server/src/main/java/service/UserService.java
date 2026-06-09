7fad81bdce41a33c1826d313921ef6f3eff0b2d5
Jonas <jjor9681@byu.edu>
2026-06-08 21:40:01 -0600

    Fixed up the register method in UserService.java.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,7 +1,12 @@
 package service;
 
+import java.util.UUID;
+
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
+import model.AuthData;
+import model.UserData;
 
 public class UserService {
 
@@ -13,8 +18,28 @@ public class UserService {
         this.authDAO = authDataAccessObject;
     }
 
-    public void register() {
+    public AuthData register(UserData userData) throws AlreadyTakenException,BadRequestException, DataAccessException {
+        // Successful registration should return an authToken and a username, so I've chosen AuthData for the return type here.
 
+        // Check for a bad request:
+        if (userData.username() == null || userData.email() == null || userData.password() == null){
+            throw new BadRequestException("Error: bad request");
+        }
+
+        // Check if a username is already taken, which I can take advantage of the fact that I used a hashmap.
+        UserData otherUser = userDAO.getUser(userData.username());
+        if (otherUser != null){
+            throw new AlreadyTakenException("Error: already taken");
+        }
+
+        // Username passed security. It can go to the database.
+        userDAO.createUser(userData);
+
+        // Time to return an authToken. This was in the phase instructions.
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, userData.username());
+        authDAO.createAuth(authData);
+        return authData;
     }
 
     public void login() {
