8d3b7f6343c1b834b82eb986959ef4b48e3c89e0
Jonas <jjor9681@byu.edu>
2026-06-09 17:13:12 -0600

    Clear handler has basic components put together.

--- server/src/main/java/handling/Clear.java
+++ server/src/main/java/handling/Clear.java
@@ -1,4 +1,28 @@
 package handling;
 
-public class Clear {
+import io.javalin.http.Handler;
+import service.DeleteService;
+import io.javalin.http.Context;
+
+import java.util.Map;
+
+public class Clear implements Handler {
+
+    private final DeleteService deleteService;
+
+    public Clear(DeleteService deleteService) {
+        this.deleteService = deleteService;
+    }
+
+    @Override
+    public void handle(Context ctx) {
+        try {
+            deleteService.delete();
+            ctx.status(200);
+        }
+        catch (Exception ex) {
+            ctx.status(500);
+            ctx.json(Map.of("message", ex.getMessage())); // I can do this instead of making a whole new class.
+        }
+    }
 }
