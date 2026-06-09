67dde296ffa94bef00da31b0e629394596edc886
Jonas <jjor9681@byu.edu>
2026-06-09 17:35:27 -0600

    Copy past framework and edits to CreateGame.java

--- server/src/main/java/handling/CreateGame.java
+++ server/src/main/java/handling/CreateGame.java
@@ -1,5 +1,42 @@
 package handling;
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import model.GameData;
+import service.BadRequestException;
+import service.GameService;
+import service.UnauthorizedException;
+import java.util.Map;
 
-public class CreateGame {
+public class CreateGame implements Handler {
 
+    private final GameService gameService;
+    private final Gson gson = new Gson();
+
+    public CreateGame(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @Override
+    public void handle(Context ctx) {
+        try {
+            String authToken = ctx.header("Authorization");
+            GameData gameData = gson.fromJson(ctx.body(), GameData.class);
+            int gameID = gameService.createGame(authToken, gameData.gameName());
+            ctx.status(200);
+            ctx.json(Map.of("gameID", gameID));
+        }
+        catch (BadRequestException ex) {
+            ctx.status(400);
+            ctx.json(Map.of("message", ex.getMessage()));
+        }
+        catch (UnauthorizedException ex) {
+            ctx.status(401);
+            ctx.json(Map.of("message", ex.getMessage()));
+        }
+        catch (Exception ex) {
+            ctx.status(500);
+            ctx.json(Map.of("message", ex.getMessage()));
+        }
+    }
 }
