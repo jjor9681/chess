38cda18123deee5f3314faad50b676c4a5002612
Jonas <jjor9681@byu.edu>
2026-06-09 17:29:50 -0600

    listgames.java complete. A little bit less complex than the others.

--- server/src/main/java/handling/ListGames.java
+++ server/src/main/java/handling/ListGames.java
@@ -1,4 +1,37 @@
 package handling;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import model.GameData;
+import service.GameService;
+import service.UnauthorizedException;
+import java.util.Collection;
+import java.util.Map;
 
-public class ListGames {
+public class ListGames implements Handler {
+
+    private final GameService gameService;
+
+    public ListGames(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @Override
+    public void handle(Context ctx) {
+        try {
+            String authToken = ctx.header("Authorization");
+            Collection<GameData> games = gameService.listGames(authToken);
+            ctx.status(200);
+            ctx.json(games);
+        }
+        catch (UnauthorizedException ex) {
+            ctx.status(401);
+            ctx.json(Map.of("message", ex.getMessage()
+            ));
+        }
+        catch (Exception ex) {
+            ctx.status(500);
+            ctx.json(Map.of("message", ex.getMessage()
+            ));
+        }
+    }
 }
