19bf17593900a23256db3d108db81972aa4ddca3
Jonas <jjor9681@byu.edu>
2026-06-08 23:06:37 -0600

    Large chunk of joinGame implemented. Not sure yet how to assign a spectator to a game.

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,93 @@
+package service;
+
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.DataAccessException;
+import java.util.Collection;
+
+
+public class GameService {
+
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    // Stack overflow has an example with an incrementing field that is used to make ID's for long lists.
+    // I'll use the same concept for making game ID's.
+    private int gameID_Assignment = 1;
+
+    public GameService(GameDAO gameDataAccessObject, AuthDAO authDataAccessObject) {
+        this.gameDAO = gameDataAccessObject;
+        this.authDAO = authDataAccessObject;
+    }
+
+    public Collection<GameData> listGames(String authToken) throws UnauthorizedException, DataAccessException {
+
+        AuthData authData = authDAO.getAuth(authToken);
+        if (authData == null) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+        return gameDAO.listGames();
+    }
+
+    public int createGame(String authToken, String gameName) throws UnauthorizedException, BadRequestException, DataAccessException {
+
+        AuthData authData = authDAO.getAuth(authToken);
+        if (authData == null) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        // Make sure the game exists.
+        if (gameName == null) {
+            throw new BadRequestException("Error: bad request");
+        }
+
+        ChessGame chessGame = new ChessGame();
+        int gameID = gameID_Assignment++;
+        GameData gameData = new GameData(gameID, null,null,gameName,chessGame);
+        gameDAO.createGame(gameData);
+
+        return gameID;
+    }
+
+    public void joinGame(String authToken, String playerColor, int gameID) throws UnauthorizedException, BadRequestException, AlreadyTakenException, DataAccessException {
+
+        AuthData authData = authDAO.getAuth(authToken);
+        if (authData == null) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+
+        // Time to retrieve some gameData using the gameID. If there is no game associated with the ID, then i'll throw.
+        GameData gameData = gameDAO.getGame(gameID);
+        if (gameData == null) {
+            throw new BadRequestException("Error: bad request");
+        }
+
+        // Team white, team black, and spectators should be able to join the game. Anything else throws.
+        // I need to check null first so that there are no crashes.
+        if (playerColor!= null && !playerColor.equals("WHITE") && !playerColor.equals("BLACK")){
+            throw new BadRequestException("Error: bad request");
+        }
+
+        // Time to make sure that someone else isn't already in the game when a new user tries to take their spot.
+        String whiteUsername = gameData.whiteUsername();
+        String blackUsername = gameData.blackUsername();
+
+        if (playerColor != null && playerColor.equals("WHITE")) {
+            if (whiteUsername != null) {
+                throw new AlreadyTakenException("Error: already taken");
+            }
+            whiteUsername = authData.username();
+        } else if (playerColor != null) {
+            if (blackUsername != null) {
+                throw new AlreadyTakenException("Error: already taken");
+            }
+            blackUsername = authData.username();
+        }
+
+        GameData newPlayerGame = new GameData(gameID, whiteUsername, blackUsername, gameData.gameName(), gameData.game());
+        gameDAO.updateGame(newPlayerGame);
+    }
+}
