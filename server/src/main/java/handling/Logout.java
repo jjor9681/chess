008a4318676d31678b7e7c3ef15e58dad3e3ff06
Jonas <jjor9681@byu.edu>
2026-06-09 17:28:07 -0600

    logout.java complete. Basically just rips away auth token.

--- server/src/main/java/handling/Logout.java
+++ server/src/main/java/handling/Logout.java
@@ -1,4 +1,34 @@
 package handling;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import service.UserService;
+import service.UnauthorizedException;
+import java.util.Map;
 
-public class Logout {
+public class Logout implements Handler {
+
+    private final UserService userService;
+
+    public Logout(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void handle(Context ctx) {
+        try {
+            String authToken = ctx.header("Authorization");
+            userService.logout(authToken);
+            ctx.status(200);
+        }
+        catch (UnauthorizedException ex) {
+            ctx.status(401);
+            ctx.json(Map.of("message", ex.getMessage()
+            ));
+        }
+        catch (Exception ex) {
+            ctx.status(500);
+            ctx.json(Map.of("message", ex.getMessage()
+            ));
+        }
+    }
 }
