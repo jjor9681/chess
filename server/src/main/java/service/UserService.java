9b28467e66b4b06a2b9cd88d621944be329969d1
Jonas <jjor9681@byu.edu>
2026-06-08 21:55:53 -0600

    Implemented login() method in UserService.java

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -35,14 +35,35 @@ public class UserService {
         // Username passed security. (The username is available.)
         userDAO.createUser(userData);
 
-        // Time to return an authToken. This was in the phase instructions.
+        // Time to return an authToken and username. This was in the phase instructions.
         String authToken = UUID.randomUUID().toString();
         AuthData authData = new AuthData(authToken, userData.username());
         authDAO.createAuth(authData);
         return authData;
     }
 
-    public void login() {
+    public AuthData login(UserData userData) throws UnauthorizedException, BadRequestException, DataAccessException {
+        // Same checks as earlier on the username.
+        if (userData.username() == null || userData.password() == null) {
+            throw new BadRequestException("Error: bad request");
+        }
+        // Basically a copy and paste from register, except we want the name to be taken for login purposes.
+        UserData otherUser = userDAO.getUser(userData.username());
+        if (otherUser == null){
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+        // See if passwords match
+        if (otherUser.password().equals(userData.password())){
+
+            // No need to create another user. That is done in register. This authData will only have the authToken that is new.
+
+            String authToken = UUID.randomUUID().toString();
+            AuthData authData = new AuthData(authToken, userData.username());
+            authDAO.createAuth(authData);
+            return authData;
+
+        } // Passwords didn't match, --> unauthorized.
+        throw new UnauthorizedException("Error: unauthorized");
 
     }
 
