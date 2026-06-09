fb49ce49c021732b44c67e1d6ec9abf29e261650
Jonas <jjor9681@byu.edu>
2026-06-08 23:54:52 -0600

    Implemented basic gson in server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,13 +1,28 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import dataaccess.*;
+import model.UserData;
+import service.*;
 
 public class Server {
 
     private final Javalin javalin;
+    private final UserDAO userDAO = new MemoryUserDAO();
+    private final AuthDAO authDAO = new MemoryAuthDAO();
+    private final GameDAO gameDAO = new MemoryGameDAO();
+    private final UserService userService = new UserService(userDAO, authDAO);
+    private final GameService gameService = new GameService(gameDAO, authDAO);
+    private final DeleteService deleteService = new DeleteService(userDAO, authDAO, gameDAO);
+    private final Gson gson = new Gson();
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalin.post("/user", ctx -> { // This is a lot of pet shop copy paste and stack overflow examples. Using a lambda just like lecture.
+            UserData userData = gson.fromJson(ctx.body(), UserData.class);
+
+        });
 
     }
 
