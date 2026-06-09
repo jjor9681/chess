5de0a911fbeac2865e01b4cc992c497c6b89fc84
Jonas <jjor9681@byu.edu>
2026-06-08 18:23:10 -0600

    Implemented createUser, which just adds the username to the database.

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,8 +1,29 @@
 package dataaccess;
 
 import model.UserData;
+import org.eclipse.jetty.server.Authentication;
 
+import java.util.HashMap;
 
-public class MemoryUserDAO {
+public class MemoryUserDAO implements UserDAO {
 
+    private final HashMap<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+
+        String username = user.username();
+
+        users.put(username, user); // put() is used with hashmaps for data entry.
+    }
+
+    @Override
+    public void getUser(String username) throws DataAccessException {
+
+    }
 }
