8cb89a59b19c2de22125a3711aacbdfefb1b4a40
Jonas <jjor9681@byu.edu>
2026-06-09 17:26:49 -0600

    Login.java complete. Basically a copy paste of register with a couple changes I pulled from my original server.java implementation with all the lambdas.

--- server/src/main/java/handling/Login.java
+++ server/src/main/java/handling/Login.java
@@ -1,4 +1,42 @@
 package handling;
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import model.AuthData;
+import model.UserData;
+import service.UserService;
+import service.BadRequestException;
+import service.UnauthorizedException;
+import java.util.Map;
 
-public class Login {
+public class Login implements Handler {
+
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public Login(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void handle(Context ctx) {
+        try {
+            UserData userData = gson.fromJson(ctx.body(), UserData.class); // I can't get away from gson here. Nothing on stack overflow provides any better way to do it.
+            AuthData authData = userService.login(userData);
+            ctx.status(200);
+            ctx.json(authData);
+        }
+        catch (BadRequestException ex) {
+            ctx.status(400);
+            ctx.json(Map.of("message", ex.getMessage()));
+        }
+        catch (UnauthorizedException ex) {
+            ctx.status(401);
+            ctx.json(Map.of("message", ex.getMessage()));
+        }
+        catch (Exception ex) {
+            ctx.status(500);
+            ctx.json(Map.of("message", ex.getMessage()));
+        }
+    }
 }
