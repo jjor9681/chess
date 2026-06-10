b01364eb2c05211d6ee78aa212f0cb5920cc5325
Jonas <jjor9681@byu.edu>
2026-06-09 18:47:38 -0600

    Basic framework for UserServiceTest.java

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,4 +1,23 @@
 package service;
 
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeEach
+    public void setup() {
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        userService = new UserService(
+                userDAO,
+                authDAO
+        );
+    }
 }
