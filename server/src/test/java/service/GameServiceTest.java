719bfb6b711a01a8a2566b536c65157b82c12922
Jonas <jjor9681@byu.edu>
2026-06-09 19:26:23 -0600

    Added positive and negative GameServiceTests

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -24,10 +24,41 @@ public class GameServiceTest {
         GameDAO gameDAO = new MemoryGameDAO();
         UserService userService = new UserService(userDAO, authDAO);
         gameService = new GameService(gameDAO, authDAO);
-        UserData userData = new UserData("bob", "password", "bob@email.com");
+        UserData userData = new UserData("Jonas", "GreenEggsAndHam", "jonas@outlook.com");
         AuthData authData = userService.register(userData);
         authToken = authData.authToken();
+    }
 
+    @Test
+    public void listGamesPositive() throws Exception {
+        gameService.createGame(authToken, "200_EloGame");
+        Collection<GameData> games = gameService.listGames(authToken);
+        assertEquals(1, games.size()); // As long as gamesTotal++ updates after the assignment, this should work.
+    }
+    @Test
+    public void listGamesNegative() {
+        assertThrows(UnauthorizedException.class, () -> gameService.listGames("SomebodyElse'sToken"));
+    }
 
+    @Test
+    public void createGamePositive() throws Exception {
+        int gameID = gameService.createGame(authToken, "200_EloGame");
+        assertTrue(gameID > 0);
+    }
+    @Test
+    public void createGameNegative() {
+        assertThrows(BadRequestException.class, () -> gameService.createGame(authToken, null));
+    }
+
+    @Test
+    public void joinGamePositive() throws Exception {
+        int gameID = gameService.createGame(authToken, "200_EloGame");
+        assertDoesNotThrow(() -> gameService.joinGame(authToken, "WHITE", gameID));
+    }
+    @Test
+    public void joinGameNegative() throws Exception {
+        int gameID = gameService.createGame(authToken, "200_EloGame");
+        gameService.joinGame(authToken, "WHITE", gameID);
+        assertThrows(AlreadyTakenException.class, () -> gameService.joinGame(authToken, "WHITE", gameID));
     }
 }
