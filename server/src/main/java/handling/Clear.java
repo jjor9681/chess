e29987305bd5562771afdbae8b30451a93d7a0ee
Jonas <jjor9681@byu.edu>
2026-06-10 21:12:41 -0600

    added clarifying comments

--- server/src/main/java/handling/Clear.java
+++ server/src/main/java/handling/Clear.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public class Clear implements Handler {
 
     private final DeleteService deleteService;
+    // Okay I need gson here to format my errors. final so that the pointer doesn't change.
     private final Gson gson = new Gson();
 
     public Clear(DeleteService deleteService) {
@@ -19,10 +20,12 @@ public class Clear implements Handler {
     @Override
     public void handle(Context ctx) {
         try {
+            // just wipes everything. 200 means it worked.
             deleteService.delete();
             ctx.status(200);
         }
         catch (Exception ex) {
+            // If something crashes, just wrap the error message in JSON. Fancy writing if I do say so myself.
             ctx.status(500);
             ctx.result(gson.toJson(Map.of("message", ex.getMessage())));
         }
