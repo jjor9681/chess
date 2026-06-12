4863617252949cae003d4bff3c8028c671ddb92c
Jonas <jjor9681@byu.edu>
2026-06-12 15:37:01 -0600

    createGame() done. Only thing so far that uses gson

--- server/src/main/java/dataaccess/sqldataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/sqldataaccess/MySqlGameDAO.java
@@ -1,5 +1,6 @@
 package dataaccess.sqldataaccess;
 
+import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import dataaccess.DatabaseManagerWrapper;
 import dataaccess.GameDAO;
@@ -13,6 +14,42 @@ public class MySqlGameDAO implements GameDAO {
     public void createGame(GameData game)
             throws DataAccessException {
 
+        String statement =
+                """
+                INSERT INTO game
+                (gameID, whiteUsername,
+                 blackUsername, gameName,
+                 gameState)
+                VALUES (?, ?, ?, ?, ?)
+                """;
+
+        String gameState =
+                // First time i've had to use some gson! Nice!
+                new Gson().toJson(game.game());
+
+        try (var conn = DatabaseManagerWrapper.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+            ps.setInt(
+                    1,
+                    game.gameID());
+            ps.setString(
+                    2,
+                    game.whiteUsername());
+            ps.setString(
+                    3,
+                    game.blackUsername());
+            ps.setString(
+                    4,
+                    game.gameName());
+            ps.setString(
+                    5,
+                    gameState);
+            ps.executeUpdate();
+        } catch (Exception ex) {
+            throw new DataAccessException(
+                    "Unable to create game",
+                    ex);
+        }
     }
 
     @Override
@@ -36,8 +73,20 @@ public class MySqlGameDAO implements GameDAO {
     }
 
     @Override
-    public void clear()
-            throws DataAccessException {
+    public void clear() throws DataAccessException {
 
+        String statement = "TRUNCATE TABLE game";
+
+        try (var conn = DatabaseManagerWrapper.getConnection();
+             var ps = conn.prepareStatement(statement)) {
+
+            ps.executeUpdate();
+
+        } catch (Exception ex) {
+
+            throw new DataAccessException(
+                    "couldn't clear the game table",
+                    ex);
+        }
     }
 }
