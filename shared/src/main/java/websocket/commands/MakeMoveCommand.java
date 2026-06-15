8f2e1cd480448f52a66ea10044696bbf3aad388c
Jonas <jjor9681@byu.edu>
2026-06-15 10:32:30 -0600

    first attempt at MakeMoveCommand.java, so hopefully it is good to go.

---
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -0,0 +1,39 @@
+// This file appears in the spec, so I will start here.
+
+package websocket.commands;
+import chess.ChessMove;
+import java.util.Objects;
+
+public class MakeMoveCommand extends UserGameCommand {
+
+    private final ChessMove move;
+
+    public MakeMoveCommand(
+            String authToken,
+            Integer gameID,
+            ChessMove move) {
+        super(CommandType.MAKE_MOVE, authToken, gameID);
+        this.move = move;
+    }
+    public ChessMove getMove() {
+        return move;
+    }
+    @Override
+    public boolean equals(Object object) {
+        if (!super.equals(object)) {
+            return false;
+        }
+        if (!(object instanceof MakeMoveCommand that)) {
+            return false;
+        }
+        return Objects.equals(
+                getMove(),
+                that.getMove());
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                super.hashCode(),
+                getMove());
+    }
+}
