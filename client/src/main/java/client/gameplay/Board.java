49d4038dfd9019c7b77fa76dcffe6013487c40ff
Jonas <jjor9681@byu.edu>
2026-06-15 14:44:54 -0600

    Board drawing and from the correct north or south perspective

--- client/src/main/java/client/gameplay/Board.java
+++ client/src/main/java/client/gameplay/Board.java
@@ -1,4 +1,34 @@
 package client.gameplay;
 
+import chess.ChessGame;
+import ui.GameBuilder;
+
 public class Board {
+
+    private final GameBuilder gameBuilder =
+            new GameBuilder();
+
+    private final String perspective;
+
+    private ChessGame currentGame;
+
+    public Board(String perspective) {
+        this.perspective = perspective;
+    }
+
+    public void updateGame(ChessGame game) {
+        currentGame = game;
+    }
+
+    public String redraw() {
+        if (currentGame == null) {
+            return "No game loaded yet.\n";
+        }
+
+        if (perspective.equals("BLACK")) {
+            return gameBuilder.buildBlackBoard(currentGame);
+        }
+
+        return gameBuilder.buildWhiteBoard(currentGame);
+    }
 }
