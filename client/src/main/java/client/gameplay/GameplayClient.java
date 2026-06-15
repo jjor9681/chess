37f8c8ba8ec3270b886286d9da894d89526923b9
Jonas <jjor9681@byu.edu>
2026-06-15 17:01:27 -0600

    Finished the main menu and all text options. Now all that's left is to hook it up to ChessClient and ClientIsLoggedIn

--- client/src/main/java/client/gameplay/GameplayClient.java
+++ client/src/main/java/client/gameplay/GameplayClient.java
@@ -1,4 +1,114 @@
+//Connects ChessClient and ClientIsLoggedIn
+
 package client.gameplay;
 
-public class GameplayClient {
+import client.websocket.NotificationHandler;
+import client.websocket.WebSocketFacade;
+
+public class GameplayClient implements NotificationHandler {
+
+    private final WebSocketFacade webSocket;
+    private final Board board;
+    private final MessageHandler messageHandler;
+    private final Moves moves;
+    private final Help help = new Help();
+
+    private final String authToken;
+    private final int gameID;
+
+    public GameplayClient(
+            String serverUrl,
+            String authToken,
+            int gameID,
+            String perspective) throws Exception {
+
+        this.authToken = authToken;
+        this.gameID = gameID;
+
+        board =
+                new Board(
+                        perspective);
+
+        messageHandler =
+                new MessageHandler(
+                        board);
+
+        webSocket =
+                new WebSocketFacade(
+                        serverUrl,
+                        this);
+
+        moves =
+                new Moves(
+                        webSocket,
+                        authToken,
+                        gameID);
+
+        webSocket.connect(
+                authToken,
+                gameID);
+    }
+
+    @Override
+    public void notify(String rawMessage) {
+        messageHandler.handle(
+                rawMessage);
+    }
+
+    public Result eval(
+            String command,
+            String[] params) throws Exception {
+
+        return switch (command) {
+            case "help" -> new Result(
+                    false,
+                    help.text());
+
+            case "redraw" -> redraw();
+
+            case "move" -> moves.move(
+                    params);
+
+            case "highlight" -> moves.highlight(
+                    params);
+
+            case "leave" -> leave();
+
+            case "resign" -> resign();
+
+            default -> new Result(
+                    false,
+                    "Unknown command. Type help.\n");
+        };
+    }
+
+    private Result redraw() {
+        return new Result(
+                false,
+                board.redraw());
+    }
+
+    private Result leave() throws Exception {
+        webSocket.leave(
+                authToken,
+                gameID);
+
+        return new Result(
+                true,
+                "Left game.\n");
+    }
+
+    private Result resign() throws Exception {
+        webSocket.resign(
+                authToken,
+                gameID);
+
+        return new Result(
+                false,
+                "Resign request sent.\n");
+    }
+
+    public String help() {
+        return help.text();
+    }
 }
--- client/src/main/java/client/gameplay/Moves.java
+++ client/src/main/java/client/gameplay/Moves.java
@@ -1,4 +1,117 @@
 package client.gameplay;
 
+import chess.ChessMove;
+import chess.ChessPosition;
+import client.websocket.WebSocketFacade;
+
 public class Moves {
+
+    private final WebSocketFacade webSocket;
+    private final String authToken;
+    private final int gameID;
+
+    public Moves(
+            WebSocketFacade webSocket,
+            String authToken,
+            int gameID) {
+
+        this.webSocket = webSocket;
+        this.authToken = authToken;
+        this.gameID = gameID;
+    }
+
+    public Result move(String[] params) throws Exception {
+        if (params.length != 2) {
+            return new Result(
+                    false,
+                    "Expected: move <start> <end>\n");
+        }
+
+        ChessPosition start =
+                parsePosition(params[0]);
+
+        ChessPosition end =
+                parsePosition(params[1]);
+
+        ChessMove move =
+                new ChessMove(
+                        start,
+                        end,
+                        null);
+
+        webSocket.makeMove(
+                authToken,
+                gameID,
+                move);
+
+        return new Result(
+                false,
+                "Move sent.\n");
+    }
+
+    public Result highlight(String[] params) {
+        if (params.length != 1) {
+            return new Result(
+                    false,
+                    "Expected: highlight <position>\n");
+        }
+
+        return new Result(
+                false,
+                "Highlight not implemented yet.\n");
+    }
+
+    private ChessPosition parsePosition(String input) throws Exception {
+        if (input.length() != 2) {
+            throw new Exception(
+                    "Position must look like e2.");
+        }
+
+        char file =
+                Character.toLowerCase(
+                        input.charAt(0));
+
+        char rank =
+                input.charAt(1);
+
+        int column =
+                stringToColumn(file);
+
+        int row =
+                stringToRow(rank);
+
+        return new ChessPosition(
+                row,
+                column);
+    }
+
+    private int stringToColumn(char file) throws Exception {
+        return switch (file) {
+            case 'a' -> 1;
+            case 'b' -> 2;
+            case 'c' -> 3;
+            case 'd' -> 4;
+            case 'e' -> 5;
+            case 'f' -> 6;
+            case 'g' -> 7;
+            case 'h' -> 8;
+            default -> throw new Exception(
+                    "Column must be a through h.");
+        };
+    }
+
+    private int stringToRow(char rank) throws Exception {
+        return switch (rank) {
+            case '1' -> 1;
+            case '2' -> 2;
+            case '3' -> 3;
+            case '4' -> 4;
+            case '5' -> 5;
+            case '6' -> 6;
+            case '7' -> 7;
+            case '8' -> 8;
+            default -> throw new Exception(
+                    "Row must be 1 through 8.");
+        };
+    }
 }
