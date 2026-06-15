aba0a279cd58621ffeb9b709c6d148d7ec8e8f0d
Jonas <jjor9681@byu.edu>
2026-06-15 10:48:09 -0600

    Added ErrorMessage.java and commented that it is specifically for the field errorMessage

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,42 @@
+// This is another phase 6 spec suggested file.
+// The field ErrorMessage is what I'm really looking for though.
+
+package websocket.messages;
+
+import java.util.Objects;
+
+public class ErrorMessage extends ServerMessage {
+
+    private final String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        if (!(o instanceof ErrorMessage that)) {
+            return false;
+        }
+
+        return Objects.equals(
+                getErrorMessage(),
+                that.getErrorMessage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                super.hashCode(),
+                getErrorMessage());
+    }
+}
