85accacc3b0e6fbcf44cbd15a61cb1fc66e01a99
Jonas <jjor9681@byu.edu>
2026-06-15 13:39:39 -0600

    Basing leave on the same folder structure for my other command files.

--- server/src/main/java/server/websocket/leave/LeaveCommunicator.java
+++ server/src/main/java/server/websocket/leave/LeaveCommunicator.java
@@ -1,4 +1,35 @@
+// notify everybody else
+// and remove the websocket connection for the player who left
+
 package server.websocket.leave;
 
+import model.AuthData;
+import org.eclipse.jetty.websocket.api.Session;
+import server.websocket.ConnectionManagerPlus;
+import websocket.commands.UserGameCommand;
+import websocket.messages.NotificationMessage;
+
 public class LeaveCommunicator {
+
+    private final ConnectionManagerPlus connectionManager;
+
+    public LeaveCommunicator(
+            ConnectionManagerPlus connectionManager) {
+
+        this.connectionManager = connectionManager;
+    }
+    public void sendLeaveMessage(
+            UserGameCommand command,
+            Session session,
+            AuthData authData) throws Exception {
+        connectionManager.broadcastToGameExcept(
+                command.getGameID(),
+                session,
+                new NotificationMessage(
+                        authData.username()
+                                + " left the game."));
+        connectionManager.remove(
+                command.getGameID(),
+                session);
+    }
 }
