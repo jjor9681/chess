4ee78d8ba6d0a3d60b8d2c863dbe06940a60edaf
Jonas <jjor9681@byu.edu>
2026-06-15 11:37:13 -0600

    Each command will be able to access the error handler and send their respective errors.

--- server/src/main/java/server/websocket/ErrorSender.java
+++ server/src/main/java/server/websocket/ErrorSender.java
@@ -1,4 +1,26 @@
+// Every command needs a consistent way to send errors.
+// that is all handled here.
+
 package server.websocket;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ErrorMessage;
 
 public class ErrorSender {
+
+    private final ConnectionManagerPlus connectionManager;
+
+    public ErrorSender(ConnectionManagerPlus connectionManager) {
+        this.connectionManager = connectionManager;
+    }
+
+    public void send(
+            Session session,
+            String message) {
+        try {
+            connectionManager.sendToOne(
+                    session,
+                    new ErrorMessage(message));
+        } catch (Exception ignored) {
+        }
+    }
 }
