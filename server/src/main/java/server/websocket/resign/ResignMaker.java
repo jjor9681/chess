45f845e2700ae964041a6c42ef376830d519de88
Jonas <jjor9681@byu.edu>
2026-06-15 13:27:48 -0600

    Basing resign on the same folder structure for my other command files.

--- server/src/main/java/server/websocket/resign/ResignMaker.java
+++ server/src/main/java/server/websocket/resign/ResignMaker.java
@@ -1,4 +1,36 @@
+// actually apply a resignation.
+// set's game over to true.
+// save updated GameData.
+
 package server.websocket.resign;
 
+import chess.ChessGame;
+import dataaccess.GameDAO;
+import model.GameData;
+
 public class ResignMaker {
+
+    private final GameDAO gameDAO;
+
+    public ResignMaker(GameDAO gameDAO) {
+        this.gameDAO = gameDAO;
+    }
+
+    public void resign(GameData gameData) throws Exception {
+
+        ChessGame game =
+                gameData.game();
+
+        game.setGameOver(true);
+
+        GameData updatedGame =
+                new GameData(
+                        gameData.gameID(),
+                        gameData.whiteUsername(),
+                        gameData.blackUsername(),
+                        gameData.gameName(),
+                        game);
+
+        gameDAO.updateGame(updatedGame);
+    }
 }
