bd63fc29645cc387f46a9a6b7a596c91fbc232b5
Jonas <jjor9681@byu.edu>
2026-06-15 14:43:24 -0600

    Help menu

--- client/src/main/java/client/gameplay/Help.java
+++ client/src/main/java/client/gameplay/Help.java
@@ -1,4 +1,17 @@
 package client.gameplay;
 
 public class Help {
+
+    public String text() {
+        return """
+                Gameplay Commands:
+                  redraw
+                  move <start> <end>
+                  resign
+                  leave
+                  highlight <position>
+                  help
+                  quit
+                """;
+    }
 }
