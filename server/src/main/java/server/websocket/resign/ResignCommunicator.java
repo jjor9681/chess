2a6e5828a92111460d1bf5686e38e37c7b5f2df8
Jonas <jjor9681@byu.edu>
2026-06-15 13:28:28 -0600

    Basing resign on the same folder structure for my other command files.

--- server/src/main/java/server/websocket/resign/ResignCommunicator.java
+++ server/src/main/java/server/websocket/resign/ResignCommunicator.java
@@ -1,4 +1,28 @@
 package server.websocket.resign;
 
+import model.AuthData;
+import server.websocket.ConnectionManagerPlus;
+import websocket.commands.UserGameCommand;
+import websocket.messages.NotificationMessage;
+
 public class ResignCommunicator {
+
+    private final ConnectionManagerPlus connectionManager;
+
+    public ResignCommunicator(
+            ConnectionManagerPlus connectionManager) {
+
+        this.connectionManager = connectionManager;
+    }
+
+    public void sendResignMessage(
+            UserGameCommand command,
+            AuthData authData) throws Exception {
+
+        connectionManager.broadcastToGame(
+                command.getGameID(),
+                new NotificationMessage(
+                        authData.username()
+                                + " resigned.")); // feels like i'm playing age of empires
+    }
 }
